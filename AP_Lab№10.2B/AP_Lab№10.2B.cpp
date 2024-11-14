b81da28e4b53fe3a7b4b2193f613281ff4d74a6f
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-11-14 06:05:53 +0200

    first commit

---
+++ AP_Lab№10.2B/AP_Lab№10.2B.cpp
@@ -0,0 +1,264 @@
+﻿// Main.cpp
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+enum Specialty { COMP_SCIENCE, INFORMATICS, MATH_ECON, PHYS_INFO, LABOR_EDU };
+string specialtyStr[] = { "Комп'ютерні науки", "Інформатика", "Математика та економіка",
+                         "Фізика та інформатика", "Трудове навчання" };
+
+struct Student {
+    string surname;
+    unsigned course;
+    Specialty specialty;
+    int physics;
+    int math;
+    union {
+        int programming;
+        int numerical_methods;
+        int pedagogy;
+    };
+};
+
+void Create(Student* s, const int N);
+void Print(Student* s, const int N);
+void Sort(Student* s, const int N);
+int BinSearch(Student* s, const int N, const string surname, const Specialty specialty, const int thirdMark);
+void BuildIndexArray(Student* s, int* indexArray, const int N);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N;
+    cout << "Введіть кількість студентів N: "; cin >> N;
+
+    Student* s = new Student[N];
+    int* indexArray = new int[N];
+
+    int menuItem;
+    do {
+        cout << "\nВиберіть дію:\n";
+        cout << " [1] - введення даних з клавіатури\n";
+        cout << " [2] - вивід даних на екран\n";
+        cout << " [3] - фізичне впорядкування даних\n";
+        cout << " [4] - створення індексного масивуу\n";
+        cout << " [5] - бінарний пошук студента\n";
+        cout << " [0] - вихід\n";
+        cout << "Введіть значення: "; cin >> menuItem;
+
+        switch (menuItem) {
+        case 1:
+            Create(s, N);
+            break;
+        case 2:
+            Print(s, N);
+            break;
+        case 3:
+            Sort(s, N);
+            break;
+        case 4:
+            BuildIndexArray(s, indexArray, N);
+            cout << "Індексний масив створено\n";
+            break;
+        case 5: {
+            string surname;
+            int ispecialty;
+            int thirdMark;
+
+            cout << "Введіть ключі пошуку:\n";
+            cout << " спеціальність (0 - Комп. науки, 1 - Інформатика, 2 - Математика та економіка, 3 - Фізика та інформатика, 4 - Трудове навчання): ";
+            cin >> ispecialty;
+
+            cin.get();
+            cin.sync();
+            cout << " прізвище: "; getline(cin, surname);
+            cout << " оцінка з профільного предмету: "; cin >> thirdMark;
+
+            int found = BinSearch(s, N, surname, (Specialty)ispecialty, thirdMark);
+            if (found != -1)
+                cout << "Знайдено студента в позиції " << found + 1 << endl;
+            else
+                cout << "Шуканого студента не знайдено" << endl;
+            break;
+        }
+        }
+    } while (menuItem != 0);
+
+    delete[] s;
+    delete[] indexArray;
+    return 0;
+}
+
+void Create(Student* s, const int N)
+{
+    int specialty;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Студент № " << i + 1 << ":\n";
+        cin.get();
+        cin.sync();
+
+        cout << " прізвище: "; getline(cin, s[i].surname);
+        cout << " курс: "; cin >> s[i].course;
+        cout << " спеціальність (0 - Комп. науки, 1 - Інформатика, 2 - Математика та економіка, 3 - Фізика та інформатика, 4 - Трудове навчання): ";
+        cin >> specialty;
+        s[i].specialty = (Specialty)specialty;
+
+        cout << " оцінка з фізики: "; cin >> s[i].physics;
+        cout << " оцінка з математики: "; cin >> s[i].math;
+
+        switch (s[i].specialty)
+        {
+        case COMP_SCIENCE:
+            cout << " оцінка з програмування: ";
+            cin >> s[i].programming;
+            break;
+        case INFORMATICS:
+            cout << " оцінка з чисельних методів: ";
+            cin >> s[i].numerical_methods;
+            break;
+        default:
+            cout << " оцінка з педагогіки: ";
+            cin >> s[i].pedagogy;
+            break;
+        }
+        cout << endl;
+    }
+}
+
+void Print(Student* s, const int N)
+{
+    cout << "================================================================================================================="
+        << endl;
+    cout << "| № |  Прізвище  | Курс |      Спеціальність      | Фізика | Математика | Програмув. | Чис. методи | Педагогіка |"
+        << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------"
+        << endl;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << "| " << setw(1) << right << i + 1 << " ";
+        cout << "| " << setw(11) << left << s[i].surname
+            << "| " << setw(5) << right << s[i].course
+            << "| " << setw(24) << left << specialtyStr[s[i].specialty]
+            << "| " << setw(7) << right << s[i].physics
+            << "| " << setw(11) << right << s[i].math;
+
+        switch (s[i].specialty)
+        {
+        case COMP_SCIENCE:
+            cout << "| " << setw(10) << right << s[i].programming
+                << " | " << setw(14) << " | " << setw(12) << " |" << endl;
+            break;
+        case INFORMATICS:
+            cout << "| " << setw(13) << " | " << setw(11) << right << s[i].numerical_methods
+                << " | " << setw(12) << " |" << endl;
+            break;
+        default:
+            cout << "| " << setw(13) << " | " << setw(14) << " | "
+                << setw(10) << right << s[i].pedagogy << " |" << endl;
+        }
+    }
+    cout << "================================================================================================================="
+        << endl;
+}
+
+void Sort(Student* s, const int N)
+{
+    Student tmp;
+    for (int i = 0; i < N - 1; i++)
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            bool needSwap = false;
+
+            if (s[j].specialty > s[j + 1].specialty)
+                needSwap = true;
+            else if (s[j].specialty == s[j + 1].specialty)
+            {
+                int mark1 = (s[j].specialty == COMP_SCIENCE) ? s[j].programming :
+                    (s[j].specialty == INFORMATICS) ? s[j].numerical_methods : s[j].pedagogy;
+
+                int mark2 = (s[j + 1].specialty == COMP_SCIENCE) ? s[j + 1].programming :
+                    (s[j + 1].specialty == INFORMATICS) ? s[j + 1].numerical_methods : s[j + 1].pedagogy;
+
+                if (mark1 > mark2)
+                    needSwap = true;
+                else if (mark1 == mark2 && s[j].surname > s[j + 1].surname)
+                    needSwap = true;
+            }
+
+            if (needSwap)
+            {
+                tmp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = tmp;
+            }
+        }
+}
+
+void BuildIndexArray(Student* s, int* indexArray, const int N)
+{
+    for (int i = 0; i < N; i++)
+        indexArray[i] = i;
+
+    for (int i = 0; i < N - 1; i++)
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            bool needSwap = false;
+
+            if (s[indexArray[j]].specialty > s[indexArray[j + 1]].specialty)
+                needSwap = true;
+            else if (s[indexArray[j]].specialty == s[indexArray[j + 1]].specialty)
+            {
+                int mark1 = (s[indexArray[j]].specialty == COMP_SCIENCE) ? s[indexArray[j]].programming :
+                    (s[indexArray[j]].specialty == INFORMATICS) ? s[indexArray[j]].numerical_methods :
+                    s[indexArray[j]].pedagogy;
+
+                int mark2 = (s[indexArray[j + 1]].specialty == COMP_SCIENCE) ? s[indexArray[j + 1]].programming :
+                    (s[indexArray[j + 1]].specialty == INFORMATICS) ? s[indexArray[j + 1]].numerical_methods :
+                    s[indexArray[j + 1]].pedagogy;
+
+                if (mark1 > mark2)
+                    needSwap = true;
+                else if (mark1 == mark2 && s[indexArray[j]].surname > s[indexArray[j + 1]].surname)
+                    needSwap = true;
+            }
+
+            if (needSwap)
+            {
+                int tmp = indexArray[j];
+                indexArray[j] = indexArray[j + 1];
+                indexArray[j + 1] = tmp;
+            }
+        }
+}
+
+int BinSearch(Student* s, const int N, const string surname, const Specialty specialty, const int thirdMark)
+{
+    int L = 0, R = N - 1, m;
+    do {
+        m = (L + R) / 2;
+        int currentMark = (s[m].specialty == COMP_SCIENCE) ? s[m].programming :
+            (s[m].specialty == INFORMATICS) ? s[m].numerical_methods : s[m].pedagogy;
+
+        if (s[m].specialty == specialty && s[m].surname == surname && currentMark == thirdMark)
+            return m;
+
+        if (s[m].specialty < specialty ||
+            (s[m].specialty == specialty && currentMark < thirdMark) ||
+            (s[m].specialty == specialty && currentMark == thirdMark && s[m].surname < surname))
+        {
+            L = m + 1;
+        }
+        else
+        {
+            R = m - 1;
+        }
+    } while (L <= R);
+
+    return -1;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,73 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№10.2B/AP_Lab№10.2B.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(StudentSystemTests)
+    {
+    public:
+        TEST_METHOD(TestSort)
+        {
+            const int N = 3;
+            Student* s = new Student[N];
+
+            s[0].surname = "Петров";
+            s[0].course = 2;
+            s[0].specialty = COMP_SCIENCE;
+            s[0].physics = 85;
+            s[0].math = 90;
+            s[0].programming = 95;
+
+            s[1].surname = "Іванов";
+            s[1].course = 1;
+            s[1].specialty = COMP_SCIENCE;
+            s[1].physics = 80;
+            s[1].math = 85;
+            s[1].programming = 95;
+
+            s[2].surname = "Сидоров";
+            s[2].course = 3;
+            s[2].specialty = INFORMATICS;
+            s[2].physics = 90;
+            s[2].math = 95;
+            s[2].numerical_methods = 88;
+
+            Sort(s, N);
+
+            Assert::AreEqual(s[0].surname, string("Іванов"));
+            Assert::AreEqual(s[1].surname, string("Петров"));
+            Assert::AreEqual(s[2].surname, string("Сидоров"));
+
+            delete[] s;
+        }
+
+        TEST_METHOD(TestBinSearch)
+        {
+            const int N = 3;
+            Student* s = new Student[N];
+
+            s[0].surname = "Іванов";
+            s[0].specialty = COMP_SCIENCE;
+            s[0].programming = 95;
+
+            s[1].surname = "Петров";
+            s[1].specialty = COMP_SCIENCE;
+            s[1].programming = 95;
+
+            s[2].surname = "Сидоров";
+            s[2].specialty = INFORMATICS;
+            s[2].numerical_methods = 88;
+
+            int found = BinSearch(s, N, "Петров", COMP_SCIENCE, 95);
+            Assert::AreEqual(1, found);
+
+            found = BinSearch(s, N, "Неіснуючий", COMP_SCIENCE, 95);
+            Assert::AreEqual(-1, found);
+
+            delete[] s;
+        }
+    };
+}
